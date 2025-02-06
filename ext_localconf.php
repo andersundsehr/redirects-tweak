223c81c0676a1f2efdcbe86b6f3bcfc8a29960e2
Lamm <s.lamm@andersundsehr.com>
2025-02-06 17:03:30 +0100

    Fix weird cache configuration

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use AUS\RedirectsTweak\Hook\TceMainHook;
-use TYPO3\CMS\Core\Cache\Backend\ApcuBackend;
+use TYPO3\CMS\Core\Cache\Backend\SimpleFileBackend;
 use TYPO3\CMS\Core\Cache\Frontend\VariableFrontend;
 use TYPO3\CMS\Redirects\Service\RedirectCacheService;
 
@@ -13,12 +13,14 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][RedirectCacheService::class] = [
     'className' => \AUS\RedirectsTweak\Service\RedirectCacheService::class
 ];
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['andersundsehr_redirects_tweak'] ??= [
-    'frontend' => VariableFrontend::class,
-    'backend' => ApcuBackend::class,
-    'groups' => [
-        'pages',
-    ]
-];
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['andersundsehr_redirects_tweak'] = array_merge([
+        'frontend' => VariableFrontend::class,
+        'backend' => SimpleFileBackend::class,
+        'groups' => [
+            'pages',
+        ]
+    ],
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['andersundsehr_redirects_tweak'] ?? [],
+);
 
 TceMainHook::register();
